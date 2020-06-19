587c4f2654123e502a65b70645dfcba9b1a940ed
Tim McCarthy <omegoshi@gmail.com>
2020-06-19 18:56:49 +0200

    change: minor clean up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,43 @@
 class HomeController < ApplicationController
   def index
-    @locations = [['Johannesburg', 'jhb'], ['Cape Town', 'cpt'], ['Durban', 'dbn']]
+    @locations = locations.pluck(:name, :value)
     @location = "jhb"
-    @time_of_day = (6..18).cover?(Time.current.hour) ? "Today" : "Tonight"
-    @quote = quotes[@location]
+    @time_of_day = (6...18).cover?(Time.current.hour) ? "Today" : "Tonight"
+    @quote = find_location(@location)[:quotes].sample
   end
 
   def quote
-    @search = quotes.keys.map(&:to_s).include?(params[:location]) ? params[:location] : 'jhb'
-    render json: { quote: quotes[@search] }
+    location = find_location(params[:location]) || find_location('jhb')
+    render json: { quote: location[:quotes].sample }
   end
 
   private
 
-  def quotes
-    @_quotes ||= {
-      'jhb': "you can buy castle lites 🥳, but can't drink them with mates 😑",
-      'dbn': "you can walk on the beach 🥳, but you can't swim in the sea 😑",
-      'cpt': "you can take a jog on the promenade 🥳, but can't buy gwaais 😑"
-  }.with_indifferent_access
+  def generate_quote(positive, not_so_positive)
+    "#{positive} 🥳, #{not_so_positive} 😑"
+  end
+
+  def find_location(location)
+    locations.find { |h| h[:value] == location }
+  end
+
+  def locations
+    [
+      {
+        name: "Johannesburg",
+        value: 'jhb',
+        quotes: [generate_quote("you can buy castle lites", "but can't drink them with mates")]
+      },
+      {
+        name: 'Cape Town',
+        value: 'cpt',
+        quotes: [generate_quote("you can take a jog on the promenade", "but can't buy gwaais")]
+      },
+      {
+        name: 'Durban',
+        value: 'dbn',
+        quotes: [generate_quote("you can walk on the beach", "but you can't swim in the sea")]
+      }
+    ]
   end
 end
